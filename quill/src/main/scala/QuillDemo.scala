c63073a5f971b5c238a1c782e59ff161b1659eb6
laogao <gaoyuxiang@gmail.com>
2022-01-12 12:24:05 +0800

    quill compose

--- quill/src/main/scala/QuillDemo.scala
+++ quill/src/main/scala/QuillDemo.scala
@@ -1,26 +1,45 @@
 import com.typesafe.config.ConfigFactory
-import zio.{ExitCode, Task}
+import zio.{ExitCode, Task, IO}
+import io.getquill.context.ZioJdbc.DataSourceLayer
+import io.getquill.{PostgresZioJdbcContext, SnakeCase}
+import zio.{Has, ULayer}
+import io.getquill.{EntityQuery, Quoted}
+
+import java.sql.SQLException
+import javax.sql.DataSource
 
 object QuillDemo extends zio.App {
 
-  val quillExample = Task {
-    import io.getquill._
-    case class Person(id: Int, firstName: String, lastName: String, age: Int)
-    val ctx = new PostgresJdbcContext(SnakeCase, "quill")
-    import ctx._
-    val named = "Joe"
-    def somePeople = quote {
-      query[Person].filter(p => p.firstName == lift(named))
-    }
+  object QuillContext extends PostgresZioJdbcContext(SnakeCase) {
+    val dataSourceLayer: ULayer[Has[DataSource]] =
+      DataSourceLayer.fromPrefix("quill").orDie
+  }
 
-    val people: List[Person] = ctx.run(somePeople)
-    println(people)
+  case class Person(id: Int, firstName: String, lastName: String, age: Int)
+  
+  import QuillContext._
+
+  object Queries {
+    val personOlderThan = quote { (age: Int) =>
+      query[Person].filter(p => p.age > age)
+    }
+    val personNamed = quote { (name: String) =>
+      query[Person].filter(p => p.firstName == name)
+    }
+    val allPersons = quote {
+      query[Person]
+    }
+  }
+
+  val composed: IO[SQLException, List[Person]] = {
+    run(Queries.personOlderThan(lift(1))).provide(QuillContext.dataSourceLayer)
   }
 
   def program =
     for {
-      _ <- quillExample
       _ <- zio.console.putStrLn("Hello, Quill!")
+      res <- composed
+      _ <- zio.console.putStrLn(res.toString)
     } yield ()
 
   override def run(args: List[String]) = program.exitCode
